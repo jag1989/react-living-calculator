f9fe7b9c3577105d8d284b0f275f2a4bc4a835f8
Jonathan Garrett <jonathan.garrett@reasondigital.com>
2017-06-02 16:52:22 +0100

    Create some style rules to make more visually appealing

--- src/index.js
+++ src/index.js
@@ -26,13 +26,16 @@ class CalcSlider extends React.Component {
             <div className="form-control">
                 <label>
                     {this.props.label}
-                    <InputRange
-                        minValue={this.props.minVal}
-                        maxValue={this.props.maxVal}
-                        step={Step}
-                        value={this.props.value}
-                        onChange={this.handleChange.bind(this)} 
-                    />
+					<p>{this.props.description}</p>
+					<div className="formSlider">
+						<InputRange
+							minValue={this.props.minVal}
+							maxValue={this.props.maxVal}
+							step={Step}
+							value={this.props.value}
+							onChange={this.handleChange.bind(this)}
+						/>
+					</div>
                 </label>
             </div>
         );
@@ -53,15 +56,16 @@ class CalcValueInput extends React.Component {
 
     render() {
         return (
-            <div className="form-control">
+            <div className="form-control form-control--half">
                 <label>
                     {this.props.label}
-                    <NumberFormat 
+					<p>{this.props.description}</p>
+                    <NumberFormat
                         allowNegative={false}
-                        thousandSeparator={true} 
-                        prefix={'£'} 
-                        value={this.props.value} 
-                        onChange={this.handleChange.bind(this)} 
+                        thousandSeparator={true}
+                        prefix={'£'}
+                        value={this.props.value}
+                        onChange={this.handleChange.bind(this)}
                     />
                 </label>
             </div>
@@ -77,41 +81,46 @@ class CalcSliders extends React.Component {
     render() {
         return (
             <div className="living-calc_sliders">
-                <CalcValueInput  
+                <CalcValueInput
                     changeValue={this.props.changeValue.bind(this)}
-                    value={this.props.MarketValue} 
+                    value={this.props.MarketValue}
                     label="Price of the Property"
+					description="Full market value or asking price"
                     propKey="MarketValue"
                 />
-                <CalcValueInput  
+                <CalcValueInput
                     changeValue={this.props.changeValue.bind(this)}
-                    value={this.props.Deposit} 
+                    value={this.props.Deposit}
                     label="Your Deposit"
+					description="Up to a maximum of 80,000"
                     propKey="Deposit"
                 />
-                <CalcSlider  
+                <CalcSlider
                     changeValue={this.props.changeValue.bind(this)}
                     minVal={40}
                     maxVal={75}
-                    value={this.props.PercentageShare} 
+                    value={this.props.PercentageShare}
                     label="Percentage Share"
+					description="The percentage of the property you will own"
                     propKey="PercentageShare"
                 />
-                <CalcSlider  
+                <CalcSlider
                     changeValue={this.props.changeValue.bind(this)}
                     minVal={20}
                     maxVal={30}
-                    value={this.props.MortgageTerm} 
+                    value={this.props.MortgageTerm}
                     label="Mortgage Period"
+					description="The amount of years you will pay the mortgage for"
                     propKey="MortgageTerm"
                 />
-                <CalcSlider  
+                <CalcSlider
                     changeValue={this.props.changeValue.bind(this)}
                     minVal={0.5}
                     maxVal={10}
                     step={0.5}
-                    value={this.props.MortgageRate} 
+                    value={this.props.MortgageRate}
                     label="Interest Rate"
+					description="The interest rate on your mortgage repayments"
                     propKey="MortgageRate"
                 />
             </div>
@@ -139,23 +148,30 @@ class CalcDataRow extends React.Component {
 // ========================================
 
 class CalcData extends React.Component {
-    render() {       
+    render() {
          return (
             <div className="living-calc_data">
 
-                <h3>Your results</h3>
-                <CalcDataRow name="Single" value={this.props.ResultSingle } />
-                <CalcDataRow name="Joint" value={this.props.ResultJoint} />
+                <h3 className="has-support">Your Results</h3>
+				<p className="living-calc_data__description">Minimum income required</p>
+				<div className="living-calc_data__container">
+					<CalcDataRow name="Single" value={this.props.ResultSingle } />
+					<CalcDataRow name="Joint" value={this.props.ResultJoint} />
+				</div>
 
-                <h3>Example mortgage structure</h3>
-                <CalcDataRow name="Mortgage Years" value={this.props.MortgageTerm + " Years"} />
-                <CalcDataRow name="Mortgage Rate" value={this.props.MortgageRate + "%"} />
-                <CalcDataRow name="Mortgage (loan) Amount" value={this.props.MortgageAmount} />
+                <h3>Example Mortgage Structure</h3>
+				<div className="living-calc_data__container">
+					<CalcDataRow name="Mortgage Years" value={this.props.MortgageTerm + " Years"} />
+					<CalcDataRow name="Mortgage Rate" value={this.props.MortgageRate + "%"} />
+					<CalcDataRow name="Mortgage (loan) Amount" value={this.props.MortgageAmount} />
+				</div>
 
-                <h3>Monthly payments</h3>
-                <CalcDataRow name="Mortgage payments/month" value={this.props.PaymentsMortgage} />
-                <CalcDataRow name="Rent payments/month" value={this.props.PaymentsRent} />
-                <CalcDataRow name="Total" value={this.props.PaymentsTotal} />
+                <h3>Monthly Payments</h3>
+				<div className="living-calc_data__container">
+					<CalcDataRow name="Mortgage payments/month" value={this.props.PaymentsMortgage} />
+					<CalcDataRow name="Rent payments/month" value={this.props.PaymentsRent} />
+					<CalcDataRow name="Total" value={this.props.PaymentsTotal} />
+				</div>
             </div>
          );
     }
@@ -169,15 +185,15 @@ class Calculator extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            ResultSingle: 0, 
-            ResultJoint: 0, 
-            MortgageTerm: 25, 
-            MortgageRate: 5, 
-            MortgageAmount: 0, 
-            PaymentsMortgage: 0, 
-            PaymentsRent: 0, 
+            ResultSingle: 0,
+            ResultJoint: 0,
+            MortgageTerm: 25,
+            MortgageRate: 5,
+            MortgageAmount: 0,
+            PaymentsMortgage: 0,
+            PaymentsRent: 0,
             PaymentsTotal: 0,
-            MarketValue: 100000,            
+            MarketValue: 100000,
             PercentageShare: 40,
             Deposit: 10000
         }
@@ -189,35 +205,35 @@ class Calculator extends React.Component {
     }
 
     calcMortgagePayments( AmountToBorrow, NumberOfPayments ) {
-        const a = AmountToBorrow; 
-        const n = NumberOfPayments;    
+        const a = AmountToBorrow;
+        const n = NumberOfPayments;
 
         let r = ( this.state.MortgageRate / 100 ) / 12;
-        let p = (a * r *Math.pow((1+r),n))/(Math.pow((1+r),n)-1); 
-        let prin = Math.round( p * 100 ) / 100; 
+        let p = (a * r *Math.pow((1+r),n))/(Math.pow((1+r),n)-1);
+        let prin = Math.round( p * 100 ) / 100;
 
         return prin;
     }
 
     render() {
         // Immutable definitions
-        const RateOnRent = 2.75; 
-        const IncomeMultiplesSingle = 4; 
-        const IncomeMultiplesJoint = 3.5; 
+        const RateOnRent = 2.75;
+        const IncomeMultiplesSingle = 4;
+        const IncomeMultiplesJoint = 3.5;
         const Locale = 'en-GB';
         const LocaleCurrency = {
-            style: 'currency', 
-            currency: 'GBP' 
+            style: 'currency',
+            currency: 'GBP'
         };
 
         // Calculations
-        let PercentageShare = this.state.PercentageShare / 100;        
+        let PercentageShare = this.state.PercentageShare / 100;
         let NumberOfPayments    = this.state.MortgageTerm * 12;
         let ShareValue          = this.state.MarketValue * PercentageShare;
         let AmountToBorrow      = ShareValue - this.state.Deposit;
-        let EquityToRent        = this.state.MarketValue - ShareValue; 
-        let RentAmount          = ( EquityToRent * ( RateOnRent / 100 ) ) / 12; 
-        let PaymentsMortgage    = this.calcMortgagePayments( AmountToBorrow, NumberOfPayments ); 
+        let EquityToRent        = this.state.MarketValue - ShareValue;
+        let RentAmount          = ( EquityToRent * ( RateOnRent / 100 ) ) / 12;
+        let PaymentsMortgage    = this.calcMortgagePayments( AmountToBorrow, NumberOfPayments );
         let ResultSingle        = ( AmountToBorrow / IncomeMultiplesSingle ) + ( 12 * RentAmount );
         let ResultJoint         = ( AmountToBorrow / IncomeMultiplesJoint ) + ( 12 * RentAmount );
         let PaymentsTotal       = RentAmount + PaymentsMortgage;
@@ -225,7 +241,7 @@ class Calculator extends React.Component {
         return (
             <div className="living-calc">
                 <div className="living-calc_body">
-                    <CalcSliders 
+                    <CalcSliders
                         changeValue={this.changeValue.bind(this)}
                         MarketValue={this.state.MarketValue}
                         PercentageShare={this.state.PercentageShare}
